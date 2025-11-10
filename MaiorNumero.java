bd8f12d98df23d0f1c6fc84537e62817e42bb57f
Nicolly-silva0 <ns158465@gmail.com>
2025-11-10 12:23:26 -0300

    Add files via upload

---
+++ MaiorNumero.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class MaiorNumero {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        if (numero1 > numero2) {
+            System.out.println("O maior número é: " + numero1);
+        } else if (numero2 > numero1) {
+            System.out.println("O maior número é: " + numero2);
+        } else {
+            System.out.println("Os dois números são iguais.");
+        }
+
+        scanner.close();
+    }
+}
